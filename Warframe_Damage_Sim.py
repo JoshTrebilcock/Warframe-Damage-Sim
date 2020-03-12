59390c13dec37ec1d5aad917c03f5f6895b78216
JoshTrebilcock <56802825+JoshTrebilcock@users.noreply.github.com>
2020-03-12 22:56:23 +0000

    additional setup + start of damage calculation

--- Warframe_Damage_Sim.py
+++ Warframe_Damage_Sim.py
@@ -3,6 +3,7 @@ weaponStats = {"baseDamage":100,
                "criticalChance":0.75,
                "criticalMultiplier":4.4,
                "statusChance":0.5,
+               "statusDuration":1,
                "impactDamage":20,
                "punctureDamage":40,
                "slashDamage":40,
@@ -23,18 +24,46 @@ weaponStats = {"baseDamage":100,
                "magazineCapacity":100,
                "reload":2.0}
 
+weaponDamageValues = {"impact":20,
+                      "puncture":40,
+                      "slash":40,
+                      "corrosive":150,}
+
 enemyStats = {"health":10000,
               "armor":1000,
               "shields":2500,
               "healthType":"clonedFlesh",
               "armorType":"ferriteArmor",
-              "shieldsType":"standardShields"}
+              "shieldsType":"normalShields"}
+
+healthTypes = {"clonedFlesh":{"impact":0.75,"puncture":1,"slash":1.25,"toxin":1,"heat":1.25,"electricity":1,"cold":1,"corrosive":1,"blast":1,"radiation":1,"viral":1.75,"gas":0.5,"magnetic":1}}
+
+shieldTypes = {"normalShields":{"impact":1.5,"puncture":0.8,"slash":1,"toxin":1,"heat":1,"electricity":1,"cold":1.5,"corrosive":1,"blast":1,"radiation":0.75,"viral":1,"gas":1,"magnetic":1.75}}
+
+armorTypes = {"ferriteArmor":{"impact":1,"puncture":1.5,"slash":0.85,"toxin":1,"heat":1,"electricity":1,"cold":1,"corrosive":1.75,"blast":0.75,"radiation":1,"viral":1.75,"gas":0.5,"magnetic":1}}
 
 timePassed = 0.0
 timeUntilNextShot = 0.0
-timeUntilNextStatus = 0.0
+timeUntilNextStatusTick = 0.0
+
+"""timeUntilNextToxinTick = 0.0
+timeUntilNextHeatTick = 0.0
+timeUntilNextGasTick = 999.0
+timeUntilNextElectricityTick = 0.0
+timeUntilNextCorrosiveExpiry = 0.0
+timeUntilNextViralExpiry = 0.0
+timeUntilNextMagneticExpiry = 0.0"""
 
 def FireShot():
+    totalDamage = 0 #possibly make global
+    if (enemyStats["shields"] > 0):
+        for damageType in weaponDamageValues:
+            print(weaponDamageValues[damageType],"*",shieldTypes[enemyStats["shieldsType"]][damageType],"=")
+            totalDamage += (weaponDamageValues[damageType]*shieldTypes[enemyStats["shieldsType"]][damageType])
+            print(totalDamage)
+    return
+
+def StatusTick():
     return
 
 def SlashTick():
@@ -46,23 +75,19 @@ def ToxinTick():
 def HeatTick():
     return
 
+def GasTick():
+    return
+
 def ElectricityTick():
     return
 
-def CorrosiveExpire():
+def CorrosiveExpiry():
     return
 
-def ViralExpire():
+def ViralExpiry():
     return
 
-def MagneticExpire():
+def MagneticExpiry():
     return
 
 FireShot()
-while(enemyStats["health"]>0):
-    enemyStats["health"] -= 499
-    print(enemyStats["health"])
-    
-
-print(weaponStats)
-print(enemyStats)
